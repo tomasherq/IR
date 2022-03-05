7b6b8abc3834aab3360ae583baf068e3c6f227c2
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-03-05 21:13:29 +0100

    Worst stats checking

--- evals/worst_queries/get_worst_stats.py
+++ evals/worst_queries/get_worst_stats.py
@@ -19,6 +19,18 @@ def read_all_document(filename):
     return queries
 
 
+def read_all_queries_complete(filename):
+
+    queries = []
+
+    with open(filename, "r") as file_read:
+
+        for line in file_read:
+            queries.append((line.split("\t")[0].strip(), line.split("\t")[1].strip()))
+
+    return queries
+
+
 def read_json(filename):
 
     with open(filename, "r") as file_read:
@@ -42,7 +54,7 @@ def get_avg_stop(queries):
 
         words = query.split(" ")
         for word in words:
-            if word in stop_words:
+            if word.lower() in stop_words:
                 counter_stop += 1
             counter_tot += 1
 
@@ -66,32 +78,37 @@ def get_stats(all, worst):
     print(f"Worst : {avg_stop_bad}\n")
 
 
-def get_term_frequency(worst_queries, worst_passages):
+def get_term_frequency(queries, passages):
 
     stop_words = read_json("stop_words_english.json")
-    index = 0
-    for query_info in worst_queries:
+
+    for query_info in queries:
         times_appear = 0
         word_least = ""
 
         queryId = query_info[0]
         query = query_info[1]
 
-        passage = worst_passages[index]
-        for term in query.split(" "):
-            if term not in stop_words:
-                if term not in passage:
-                    times_appear = 0
-                    word_least = term
-                    break
-                number_apperances = len(passage.split(term))
+        for passageInfo in passages:
+            if passageInfo[0] == queryId:
+                passage = passageInfo[1].lower()
+                for term in query.strip().split(" "):
+                    term = term.lower()
 
-                if number_apperances < times_appear or times_appear == 0:
-                    word_least = term
-                    times_appear = number_apperances
-        index += 1
-        print(
-            f"For the query {queryId} the word that appears the least is {word_least} and it appears {times_appear} times")
+                    if term not in stop_words and len(term):
+
+                        if term not in passage:
+                            times_appear = 0
+                            word_least = term
+                            break
+                        number_apperances = len(passage.split(term))
+
+                        if number_apperances < times_appear or times_appear == 0:
+                            word_least = term
+                            times_appear = number_apperances
+
+                print(
+                    f"For the query {queryId} the word that appears the least is {word_least} and it appears {times_appear} times")
 
 
 # NB: postings will be null if the document is empty
@@ -118,9 +135,11 @@ for passage_info in read_json(ALL_PASSAGES):
 print("Stats for passages")
 get_stats(all_passages_info, worst_passages)
 
+print("Worst queries")
 get_term_frequency(worst_queries_read, worst_passages_read)
 
-get_term_frequency(read_json(ALL_PASSAGES), read_all_document(QUERIES_FILE))
+print("\nAll passages")
+get_term_frequency(read_all_queries_complete(QUERIES_FILE), read_json(ALL_PASSAGES))
 
 # Documents
 
