c4ca135b74e0945b10ab088503b80d26e908e7a4
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-03-05 21:28:10 +0100

    File python

--- models/common.py
+++ models/common.py
@@ -3,24 +3,23 @@ import os
 import matplotlib.pyplot as plt
 import json
 
-INDEX = "/home/tomas/Escritorio/IR/project/IR/createIndex/passage_index"
+os.environ['JAVA_HOME'] = 'C:\Program Files\Java\jdk-11.0.8'
+INDEX = "C:/Users/murta/Desktop/IR/CoreIR/Group/terrier-project-5.5-bin.tar/terrier-project-5.5/var/index/passage_index"
 OUTPUT = f"../runs/run_{len(os.listdir('../runs'))}.txt"
-# TOPICS_FILE = "/home/tomas/Escritorio/IR/project/IR/queries/msmarco-test2019-queries.tsv"
-TOPICS_FILE = "/home/tomas/Escritorio/IR/project/IR/queries/queries.dev.tsv"
-
-# QRELS_FILE = "/home/tomas/Escritorio/IR/project/IR/queries/2019qrels-pass.txt"
-QRELS_FILE = "/home/tomas/Escritorio/IR/project/IR/queries/qrels.dev.tsv"
-
-OUTPUT_EVAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_per_query_dev.json"
-OUTPUT_EVAL_GENERAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_general_dev.json"
+TOPICS_FILE = "../queries/queries.dev.tsv" # <-- BM25
+TRAIN_TOPICS_FILE = "C:/Users/murta/Desktop/IR/CoreIR/Group/collectionandqueries/queries.train.tsv" # <-- L2R
+TEST_TOPICS_FILE = "C:/Users/murta/Desktop/IR/CoreIR/Group/collectionandqueries/queries.eval.tsv" # <-- L2R
+QRELS_FILE = "../queries/qrels.dev.tsv"
+OUTPUT_EVAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_per_query.json"
+OUTPUT_EVAL_GENERAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_general.json"
 pt.init(boot_packages=["com.github.terrierteam:terrier-prf:-SNAPSHOT"])
 
 topics = pt.io.read_topics(TOPICS_FILE, format="singleline")
-
-
+train_topics = pt.io.read_topics(TRAIN_TOPICS_FILE, format="singleline")
+test_topics = pt.io.read_topics(TEST_TOPICS_FILE, format="singleline")
 qrels = pt.io.read_qrels(QRELS_FILE)
 
 
 def write_eval(dictionary, outputFile):
-    with open(outputFile, "w") as file_write:
+    with open(outputFile, "w", encoding="utf-8") as file_write:
         file_write.write(json.dumps(dictionary, indent=4))
--- models/learning_rank.py
+++ models/learning_rank.py
@@ -1,36 +1,14 @@
 from sklearn.ensemble import RandomForestRegressor
-from common import *
+from common_mo import *
 import sys
 
 pipeline = pt.FeaturesBatchRetrieve(INDEX, wmodel="BM25", features=["WMODEL:Tf", "WMODEL:PL2"])
-# bm25 = pt.BatchRetrieve(INDEX, wmodel="BM25", controls={"c": 0.6, "bm25.k_1": 0.6, "bm25.k_3": 0.5})
+bm25 = pt.BatchRetrieve(INDEX, wmodel="BM25")
 
-rf = RandomForestRegressor(n_estimators=100)
+rf = RandomForestRegressor(n_estimators=400)
 rf_pipe = pipeline >> pt.ltr.apply_learned_model(rf)
-rf_pipe.fit(topics, qrels)
+rf_pipe.fit(train_topics, qrels)
 
+results = pt.Experiment([bm25, rf_pipe], test_topics, qrels, ['map', "ndcg", "recip_rank"], names=["BM25 Baseline", "LTR"])
 
-rtr = pt.Experiment([rf_pipe], topics, qrels, ['map', "ndcg", "recip_rank"],
-                    names=["LTR"], perquery=True)
-
-results = rtr.to_json(orient="table")
-
-OUTPUT_EVAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_per_query_ltr.json"
-OUTPUT_EVAL_GENERAL = f"../evals/run_{len(os.listdir('../runs'))}_eval_general_ltr.json"
-
-
-write_eval(results, OUTPUT_EVAL)
-
-
-rtr = pt.Experiment([rf_pipe], topics, qrels, ['map', "ndcg", "recip_rank"],
-                    names=["LTR"], perquery=False)
-
-results = rtr.to_json(orient="table")
-
-write_eval(results, OUTPUT_EVAL_GENERAL)
-
-
-pt.io.write_results(rtr, OUTPUT, format='trec', append=False)
-
-
-# print(results)
+print(results)
